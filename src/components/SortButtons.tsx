6d328c5bdfc84a57836ba54e07e61498cb94b15b
Dillan Prince <dillanprince@gmail.com>
2023-09-10 16:15:55 -0500

    refactor buttons to reduce duplication

--- src/components/SortButtons.tsx
+++ src/components/SortButtons.tsx
@@ -1,26 +1,31 @@
 import { Button, ButtonGroup } from "@mui/material";
 import { grey } from "@mui/material/colors";
-import { useMonsterContext } from "../contexts/MonsterContext";
+import { PropsWithChildren } from "react";
+import { SortMethod, useMonsterContext } from "../contexts/MonsterContext";
 
-const SortButtons = () => {
-  const { isLoading, sortMethod, setSortMethod } = useMonsterContext();
+type SortButtonProps = {
+  sortMethod: SortMethod;
+};
+
+const SortButton = ({ sortMethod, children }: PropsWithChildren<SortButtonProps>) => {
+  const { isLoading, sortMethod: selectedSortMethod, setSortMethod } = useMonsterContext();
 
   return (
+    <Button
+      disabled={isLoading}
+      sx={{ flex: 1, backgroundColor: sortMethod === selectedSortMethod ? grey[300] : "inherit" }}
+      onClick={() => setSortMethod(sortMethod)}
+    >
+      {children}
+    </Button>
+  );
+};
+
+const SortButtons = () => {
+  return (
     <ButtonGroup sx={{ display: "flex" }}>
-      <Button
-        disabled={isLoading}
-        sx={{ flex: 1, backgroundColor: sortMethod === "ID" ? grey[300] : "inherit" }}
-        onClick={() => setSortMethod("ID")}
-      >
-        ID
-      </Button>
-      <Button
-        disabled={isLoading}
-        sx={{ flex: 1, backgroundColor: sortMethod === "ALPHABETICAL" ? grey[300] : "inherit" }}
-        onClick={() => setSortMethod("ALPHABETICAL")}
-      >
-        Alphabetical
-      </Button>
+      <SortButton sortMethod="ID">ID</SortButton>
+      <SortButton sortMethod="ALPHABETICAL">Alphabetical</SortButton>
     </ButtonGroup>
   );
 };
